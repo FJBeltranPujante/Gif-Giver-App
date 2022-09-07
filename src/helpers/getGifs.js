8a140d044970988540a52cff1eea3ee303f0b4fc
FJBeltranPujante <kvortvaj@gmail.com>
2022-09-07 23:50:43 +0200

    first commit

--- src/helpers/getGifs.js
+++ src/helpers/getGifs.js
@@ -1,13 +1,13 @@
 export const getGifs = async( category ) => {
 
-    const url =`https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn`;
+    const url =`https://api.giphy.com/v1/gifs/search?api_key=kaJ1JwD4CuQgYun7YpTQpTr5p1qs1sQn&q=${ category }&limit=8`;
     const resp = await fetch( url );
     const { data } = await resp.json();
 
     const gifs = data.map( img => ({
         id: img.id,
         title: img.title,
-        url: img.images.downsized_medium.url
+        url: img.images.downsized.url
     }));
     
     return gifs;
